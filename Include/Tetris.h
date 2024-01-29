a37e3546de98e1974a4c09ab18c0868a9fa5453a
Mezzancolle <giuseppepugliese00@gmail.com>
2024-01-29 12:39:57 +0100

    First Commit

---
+++ Include/Tetris.h
@@ -0,0 +1,395 @@
+#ifndef TETRIS_H
+#define TETRIS_H
+
+#include <raylib.h>
+#include <time.h>
+#include <string.h>
+
+#define STAGE_WIDTH 12
+#define STAGE_HEIGHT 22
+#define TILE_SIZE 24
+#define TETROMINO_SIZE 4
+
+typedef enum { MAIN_MENU, GAMEPLAY, GAME_OVER } GameState;
+
+int stage[] = 
+{
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+};
+
+const int lTetromino0[] =
+{
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 0, 0, 0,
+};
+
+const int lTetromino90[] =
+{
+    0, 0, 0, 0,
+    1, 1, 1, 0,
+    1, 0, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int lTetromino180[] =
+{
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int lTetromino270[] =
+{
+    0, 0, 1, 0,
+    1, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int jTetromino0[] =
+{
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int jTetromino90[] =
+{
+    1, 0, 0, 0,
+    1, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int jTetromino180[] =
+{
+    0, 1, 1, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int jTetromino270[] =
+{
+    0, 0, 0, 0,
+    1, 1, 1, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 0,
+};
+
+
+const int oTetromino[] =
+{
+    1, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int sTetromino0[] =
+{
+    0, 0, 0, 0,
+    0, 1, 1, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int sTetromino90[] =
+{
+    0, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 0, 1, 0,
+};
+
+const int sTetromino180[] =
+{
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+    0, 1, 1, 0,
+    1, 1, 0, 0,
+};
+
+const int sTetromino270[] =
+{
+    0, 0, 0, 0,
+    1, 0, 0, 0,
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+};
+
+
+const int tTetromino0[] =
+{
+    0, 0, 0, 0,
+    1, 1, 1, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int tTetromino90[] =
+{
+    0, 1, 0, 0,
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int tTetromino180[] =
+{
+    0, 1, 0, 0,
+    1, 1, 1, 0,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int tTetromino270[] =
+{
+    1, 0, 0, 0,
+    1, 1, 0, 0,
+    1, 0, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int iTetromino0[] =
+{
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+};
+
+const int iTetromino90[] =
+{
+    0, 0, 0, 0,
+    1, 1, 1, 1,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int iTetromino180[] =
+{
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 1, 0, 0,
+};
+
+const int iTetromino270[] =
+{
+    0, 0, 0, 0,
+    1, 1, 1, 1,
+    0, 0, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int zTetromino0[] =
+{
+    0, 0, 0, 0,
+    1, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 0, 0, 0,
+};
+
+const int zTetromino90[] =
+{
+    0, 1, 0, 0,
+    0, 1, 1, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 0,
+};
+
+const int zTetromino180[] =
+{
+    0, 0, 0, 0,
+    0, 1, 1, 0,
+    1, 1, 0, 0,
+    0, 0, 0, 0,
+};
+
+const int zTetromino270[] =
+{
+    1, 0, 0, 0,
+    1, 1, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 0, 0,
+};
+
+const Color colorTypes[8] =
+{
+    {224, 224, 224, 255},
+    {135, 206, 235, 255},
+    {152, 251, 152, 255},
+    {255, 222, 173, 255},
+    {250, 128, 114, 255},
+    {255, 192, 203, 255},
+    {173, 216, 230, 255},
+    {255, 228, 181, 255}
+};
+
+const int *tetrominoTypes[7][4] =
+{
+    {zTetromino0, zTetromino90, zTetromino180, zTetromino270},
+    {sTetromino0, sTetromino90, sTetromino180, sTetromino270},
+    {tTetromino0, tTetromino90, tTetromino180, tTetromino270},
+    {oTetromino, oTetromino, oTetromino, oTetromino},
+    {iTetromino0, iTetromino90, iTetromino180, iTetromino270},
+    {jTetromino0, jTetromino90, jTetromino180, jTetromino270},
+    {lTetromino0, lTetromino90, lTetromino180, lTetromino270},
+};
+
+void DrawTetromino(const Color currentColor, const int startOffsetX, const int startOffsetY, const int tetrominoStartX, const int tetrominoStartY, const int *tetromino)
+{
+    for(int y = 0; y < TETROMINO_SIZE; y++)
+    {
+        for(int x = 0; x < TETROMINO_SIZE; x++)
+        {
+            const int offset = y * TETROMINO_SIZE + x;
+
+            if(tetromino[offset] == 1)
+            {
+                DrawRectangle((x + tetrominoStartX) * TILE_SIZE + startOffsetX, (y + tetrominoStartY) * TILE_SIZE + startOffsetY, TILE_SIZE, TILE_SIZE, currentColor);
+            }
+        }
+    }
+}
+
+int CheckCollision(const int tetrominoStartX, const int tetrominoStartY, const int *tetromino)
+{
+    for(int y = 0; y < TETROMINO_SIZE; y++)
+    {
+        for(int x = 0; x < TETROMINO_SIZE; x++)
+        {
+            const int offset = y * TETROMINO_SIZE + x;
+
+            if(tetromino[offset] == 1)
+            {
+                const int offset_stage = (y + tetrominoStartY) * STAGE_WIDTH + (x +tetrominoStartX);
+
+                if (stage[offset_stage] != 0)
+                {
+                    return 1;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+void ResetLines(int startLineY)
+{
+    for (int y = startLineY; y >= 0; y--)
+    {
+        for (int x = 1; x < STAGE_WIDTH - 1; x++)
+        {
+            const int offset = y * STAGE_WIDTH + x;
+            const int offset_below = (y+1) * STAGE_WIDTH + x;
+
+            if (stage[offset_below] == 0 && stage[offset] > 0)
+            {
+                stage[offset_below] = stage[offset];
+                stage[offset] = 0;
+            }
+        }
+    }   
+}
+
+void DeleteLines(int *score, float *moveTetrominoDownTimer, float *timeToMoveTetrominoDown, bool *flashActive, int *flashFrameCount, Sound lineClearSound)
+{
+    for (int y = 0; y < STAGE_HEIGHT - 1; y++)
+    {
+        int checkLine = 1;
+
+        for (int x = 1; x < STAGE_WIDTH - 1; x++)
+        {
+            const int offset = y * STAGE_WIDTH + x;
+
+            if (stage[offset] == 0)
+            {
+                checkLine = 0;
+                break;
+            }
+        }
+
+        if (checkLine)
+        {
+            const int offset = y * STAGE_WIDTH + 1;
+            memset(stage + offset, 0, (STAGE_WIDTH - 2) * sizeof(int));
+
+            ResetLines(y);
+
+            *flashActive = true;
+            *flashFrameCount = 0;
+
+            *score += 100;
+
+            *moveTetrominoDownTimer -= 0.1f;
+
+            if ((*moveTetrominoDownTimer) < 0.1f)
+            {
+                (*moveTetrominoDownTimer) = 0.1f;
+            }
+
+            (*timeToMoveTetrominoDown) = (*moveTetrominoDownTimer);
+
+            PlaySound(lineClearSound);
+        }
+    }
+}
+
+void DrawFlashEffect(int frameCount, int windowWidth, int windowHeight, bool *flashActive)
+{
+    if (frameCount % 2 == 0)
+    {
+        DrawRectangle(0, 0, windowWidth, windowHeight, Fade(RAYWHITE, 0.5f));
+    }
+
+    if (frameCount > 10)
+    {
+        *flashActive = false;
+    }
+}
+
+void ResetGame(float *moveTetrominoDownTimer, float *timeToMoveTetrominoDown, int *score, bool *flashActive, int *flashFrameCount, bool *gameReset)
+{
+    *score = 0;
+    *flashActive = false;
+    *flashFrameCount = 0;
+    *moveTetrominoDownTimer = 1.0f;
+    *timeToMoveTetrominoDown = *moveTetrominoDownTimer;
+    *gameReset = false;
+
+    // Riporta l'array stage allo stato iniziale
+    for (int i = 0; i < STAGE_WIDTH * STAGE_HEIGHT; i++)
+    {
+        stage[i] = (i % STAGE_WIDTH == 0 || i % STAGE_WIDTH == STAGE_WIDTH - 1 || i / STAGE_WIDTH == STAGE_HEIGHT - 1) ? 1 : 0;
+    }
+}
+
+#endif 
---
+++ Main_Tetris.c
@@ -0,0 +1,261 @@
+#include <Tetris.h>
+
+int main(int argc, char** argv, char** environ)
+{
+    TraceLog(LOG_INFO, "Number of arguments : %d", argc);
+
+    for(int i = 0; i < argc; i++)
+    {
+        TraceLog(LOG_INFO, "Argument : %d = %s", i, argv[i]);
+    }
+
+    while(*environ != 0)
+    {
+        TraceLog(LOG_INFO, "Argument : %s", *environ);
+        environ++;
+    }
+
+    int score = 0;
+    int flashFrameCount = 0;
+    bool flashActive = false;
+    bool gameReset = false;
+
+    //Stage Variables
+    const int windowWidth = 600; 
+    const int windowHeight = 700; 
+
+    const int startOffsetX = (windowWidth / 2) - ((STAGE_WIDTH * TILE_SIZE) / 2);
+    const int startOffsetY = (windowHeight / 2) - ((STAGE_HEIGHT * TILE_SIZE) / 2);
+
+    const int tetrominoStartX = (STAGE_WIDTH / 2) -1;
+    const int tetrominoStartY = 0;
+
+    //SpawnTetrominoPosition
+    int currentTetrominoX = tetrominoStartX;
+    int currentTetrominoY = tetrominoStartY;
+
+    //RandomTetromino
+    time_t unixTime;
+    time(&unixTime);
+    SetRandomSeed(unixTime);
+
+    int currentTetrominoType = GetRandomValue(0, 6);
+    int currentRotation = 0;
+    int currentColor = GetRandomValue(0, 7);
+    
+    //Background Music
+    InitAudioDevice(); 
+    Music music = LoadMusicStream("Music/Background.mp3");
+    PlayMusicStream(music);
+    float timePlayed = 0.0f; 
+
+    //Move Tetromino Sound
+    Sound MovetetrominoSound = LoadSound("Music/MoveTetromino.mp3");
+
+    //Delete Lines Sound
+    Sound lineClearSound = LoadSound("Music/LineClear.mp3");
+
+    //TetrominoSpeed
+    float moveTetrominoDownTimer = 1.f;
+    float timeToMoveTetrominoDown = moveTetrominoDownTimer;
+
+    //Window
+    InitWindow(windowWidth, windowHeight, "Title");
+    SetTargetFPS(60);
+
+    GameState currentGameState = MAIN_MENU;
+    
+    while(!WindowShouldClose())
+    {
+        if (currentGameState == MAIN_MENU)
+        {
+            BeginDrawing();
+            ClearBackground(RAYWHITE); // Usa il colore del tuo gioco
+
+            DrawText("Press ENTER to Play or ESC to Quit", 100, 200, 20, BLACK);
+
+            EndDrawing();
+
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentGameState = GAMEPLAY;
+                gameReset = true;
+                // Altre inizializzazioni necessarie per il gioco
+            }
+            else if (IsKeyPressed(KEY_ESCAPE))
+            {
+                break; // Esci dal gioco se viene premuto ESC nel menu principale
+            }
+        }
+        else if (currentGameState == GAMEPLAY)
+        {
+            if (gameReset)
+            {
+                ResetGame(&moveTetrominoDownTimer, &timeToMoveTetrominoDown, &score, &flashActive, &flashFrameCount, &gameReset);
+            }
+
+            //Music
+            UpdateMusicStream(music);
+            timePlayed = GetMusicTimePlayed(music)/GetMusicTimeLength(music);
+            if (timePlayed > 1.0f) timePlayed = 1.0f;
+
+            //VelocityTetromino
+            timeToMoveTetrominoDown -= GetFrameTime();
+
+            //Inputs
+            if (IsKeyPressed(KEY_SPACE))
+            {
+                PlaySound(MovetetrominoSound);
+                const int lastRotation = currentRotation;
+
+                currentRotation++;
+
+                if (currentRotation > 3)
+                {
+                    currentRotation = 0;
+                }
+
+                if (CheckCollision(currentTetrominoX,currentTetrominoY,tetrominoTypes[currentTetrominoType][currentRotation]))
+                {
+                    currentRotation = lastRotation;
+                }
+            }
+
+            if (IsKeyPressed(KEY_RIGHT))
+            {
+                PlaySound(MovetetrominoSound);
+                // No need to check overflow, wall is your protector
+                if (!CheckCollision(currentTetrominoX+1,currentTetrominoY,tetrominoTypes[currentTetrominoType][currentRotation]))
+                {
+                    currentTetrominoX++;
+                }
+            }
+            if (IsKeyPressed(KEY_LEFT))
+            {
+                PlaySound(MovetetrominoSound);
+                // No need to check overflow, wall is your protector
+                if (!CheckCollision(currentTetrominoX-1,currentTetrominoY,tetrominoTypes[currentTetrominoType][currentRotation]))
+                {
+                    currentTetrominoX--;
+                }
+            }
+
+            if(timeToMoveTetrominoDown <= 0 || IsKeyPressed(KEY_DOWN))
+            {        
+                PlaySound(MovetetrominoSound);
+
+                if(!CheckCollision(currentTetrominoX,currentTetrominoY+1,tetrominoTypes[currentTetrominoType][currentRotation]))
+                {
+                    currentTetrominoY++;
+                    timeToMoveTetrominoDown = moveTetrominoDownTimer;
+                }
+
+                else
+                {
+                    // const int currentTetrominoOffset = currentTetrominoY * STAGE_WIDTH + currentTetrominoX;
+                    
+                    // stage[currentTetrominoOffset] = 1;
+
+                    for(int y = 0; y < TETROMINO_SIZE; y++)
+                    {
+                        for(int x = 0; x < TETROMINO_SIZE; x++)
+                        {
+                            const int offset = y * TETROMINO_SIZE + x;
+
+                            const int *tetromino = tetrominoTypes[currentTetrominoType][currentRotation];
+
+                            if(tetromino[offset] == 1)
+                            {
+                                const int offset_stage = (y + currentTetrominoY) * STAGE_WIDTH + (x + currentTetrominoX);
+
+                                stage[offset_stage] = currentColor+1;
+                            }
+                        }
+                    }
+
+                    //Score and Reset
+                    DeleteLines(&score, &moveTetrominoDownTimer, &timeToMoveTetrominoDown, &flashActive, &flashFrameCount, lineClearSound);
+                    
+                    currentTetrominoX = tetrominoStartX;
+                    currentTetrominoY = tetrominoStartY;
+
+                    currentTetrominoType = GetRandomValue(0, 6);
+                    currentRotation = 0;
+                    currentColor = GetRandomValue(0, 7);
+
+                    if (CheckCollision(currentTetrominoX, currentTetrominoY, tetrominoTypes[currentTetrominoType][currentRotation]))
+                    {
+                        currentGameState = GAME_OVER;
+                        gameReset = true;
+                    }
+                }
+            }
+
+            BeginDrawing();
+            ClearBackground(GRAY);
+
+            //Draw Stage, Tetromino, Text
+            for(int y = 0; y < STAGE_HEIGHT; y++)
+            {
+                for(int x = 0; x < STAGE_WIDTH; x++)
+                {
+                    const int offset = y * STAGE_WIDTH + x;
+                    const int color = stage[offset];
+
+                    if(stage[offset] != 0)
+                    {
+                        DrawRectangle(x * TILE_SIZE + startOffsetX, y * TILE_SIZE + startOffsetY, TILE_SIZE, TILE_SIZE, colorTypes[color-1]);
+                    }
+
+                    DrawRectangleLines(x * TILE_SIZE + startOffsetX, y * TILE_SIZE + startOffsetY, TILE_SIZE, TILE_SIZE, BLACK);
+                }
+            }
+            
+            DrawTetromino(colorTypes[currentColor],startOffsetX, startOffsetY, currentTetrominoX, currentTetrominoY, tetrominoTypes[currentTetrominoType][currentRotation]);
+
+            DrawText(TextFormat("Score: %04d", score), 10, 10, 20, BLACK);
+
+            if (flashActive)
+            {
+                flashFrameCount++;
+                DrawFlashEffect(flashFrameCount, windowWidth, windowHeight, &flashActive);
+            }
+
+            EndDrawing();
+        }
+        else if (currentGameState == GAME_OVER)
+        {
+            // Schermata "Game Over"
+            BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            DrawText("Game Over", windowWidth / 2 - MeasureText("Game Over", 40) / 2, windowHeight / 2 - 20, 40, RED);
+            DrawText("Press ENTER to Play Again or ESC to Quit", 100, windowHeight - 100, 20, BLACK);
+
+            EndDrawing();
+
+            if (IsKeyPressed(KEY_ENTER))
+            {
+                currentGameState = MAIN_MENU;
+                score = 0;
+                flashActive = false;
+                flashFrameCount = 0;
+            }
+            else if (IsKeyPressed(KEY_ESCAPE))
+            {
+                break;
+            }
+        }
+
+    }
+
+    UnloadMusicStream(music);
+    UnloadSound(MovetetrominoSound);
+    UnloadSound(lineClearSound);   
+
+    CloseAudioDevice();
+
+    CloseWindow();
+
+    return 0;
+}
